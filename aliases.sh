d6dd44c924d0d8c3054135c2660b567f23b6125b
Gerard Adamkiewicz <69475021+Sharashino@users.noreply.github.com>
2022-01-26 10:32:09 +0100

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,36 @@
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+alias ct='cd C:/Repos/CyberTown/cybertown'
+alias pb='cd C:/Repos/Portobello/PortobelloDemo'
+alias sd='cd C:/Repos/Seedling/Seedling'
+alias gf='git fetch --all'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+alias gpu='git push --set-upstream origin'
+alias gpl='git pull'
+alias co='git checkout'
+alias cob='git checkout'
+alias stash='git stash'
+alias pop='git statsh pop'
+alias s='git status'
+alias last='git log -1 HEAD --stat'
+alias tree='git log --pretty=format:\"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\" --abbrev-commit -30'
+
+case "$TERM" in
+xterm*)
+	# The following programs are known to require a Win32 Console
+	# for interactive usage, therefore let's launch them through winpty
+	# when run inside `mintty`.
+	for name in node ipython php php5 psql python2.7
+	do
+		case "$(type -p "$name".exe 2>/dev/null)" in
+		''|/usr/bin/*) continue;;
+		esac
+		alias $name="winpty $name.exe"
+	done
+	;;
+esac
